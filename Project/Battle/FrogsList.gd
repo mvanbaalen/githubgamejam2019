ec1791a83630352bde1a678020b9dd002e13297d
mystic <mystic83@gmail.com>
2019-12-01 01:15:00 -0500

    add legal position check and victory condition

--- Project/Battle/FrogsList.gd
+++ Project/Battle/FrogsList.gd
@@ -12,7 +12,7 @@ func closest_enemy(asking_frog):
 	var closest_enemy = null
 	var closest_distance = null
 	for frog in get_children():
-		if asking_frog.team != frog.team:
+		if asking_frog.team != frog.team && frog.current_state != frog.State.FALLEN:
 			var distance = asking_frog.position.distance_to(frog.position)
 			if closest_distance == null or distance < closest_distance:
 				closest_distance = distance
@@ -20,8 +20,17 @@ func closest_enemy(asking_frog):
 	return closest_enemy
 	
 func check_victory():
-	var check_team = get_child(0).team
+	# TODO Always track these?
+	var player_team = []
+	var enemy_team = []
 	for frog in get_children():
-		if frog.team != check_team:
-			return false
-	return check_team
+		if frog.current_state != frog.State.FALLEN:
+			if frog.team == frog.Team.PLAYER:
+				player_team.append(frog)
+			else:
+				enemy_team.append(frog)
+	if player_team.size() > 0 and enemy_team.size() > 0:
+		return false
+	else:
+		return true
+		# This is really dumb but I am so tired
--- Project/Battle/frog/Frog.gd
+++ Project/Battle/frog/Frog.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-enum State {PREPARE, WAIT, MOVING, MELEE, DIZZY}
+enum State {PREPARE, WAIT, MOVING, MELEE, DIZZY, FALLEN}
 enum Team  {PLAYER, ENEMY}
 
 export(Team) var team = Team.PLAYER
@@ -9,6 +9,7 @@ var stats # Comes from the Abstract Frog
 
 #Prepare only
 var dragging = false
+var legal_position = false
 
 #TODO: Use timers for this
 var attack_cooldown = 2.0
@@ -27,10 +28,10 @@ func _ready():
 func _process(delta):
 	if current_state == State.PREPARE:
 		if dragging:
-			position = get_viewport().get_mouse_position()
+			calculate_dragging()
 	else:
 		current_cooldown = clamp(current_cooldown - delta, 0, attack_cooldown)
-		if current_target == null || !is_instance_valid(current_target):
+		if !valid_target():
 			find_target()
 
 func _physics_process(delta):
@@ -38,16 +39,28 @@ func _physics_process(delta):
 		State.WAIT:
 			pass
 		State.MOVING:
-			move_towards(current_target, delta)
+			move_towards_target(delta)
 		State.MELEE:
 			if can_attack(current_target):
-				attack(current_target, delta)
+				attack_target(delta)
 		State.DIZZY:
 			dizzy_time = clamp(dizzy_time - delta, 0, dizzy_cooldown)
 			if dizzy_time == 0:
 				dizzy_time = dizzy_cooldown
 				become_undizzy()
+				
+func valid_target():
+	if current_target == null:
+		return false
+	if current_target.current_state == State.FALLEN:
+		return false
+	if !is_instance_valid(current_target):
+		return false
+	return true
 
+func calculate_dragging():
+	position = get_viewport().get_mouse_position()
+	
 func start_battle():
 	current_state = State.WAIT
 	
@@ -56,29 +69,30 @@ func find_target():
 	current_state = State.MOVING
 	
 func can_attack(target):
-	if (is_instance_valid(target)
-	&& current_cooldown == 0):
+	if (valid_target() and current_cooldown == 0):
 		return true
 	return false
 	#TODO Check distance, etc.
 	
-func move_towards(target, delta):
-	rotation = position.angle_to_point(target.position)
-	var collision = move_and_collide((target.position - position).normalized() * speed * delta)
-	if collision:
-		if collision.collider == target:
-			current_state = State.MELEE
-		else:
-			if collision.collider.team != team:
-				current_target = collision.collider
+func move_towards_target(delta):
+	if valid_target():
+		rotation = position.angle_to_point(current_target.position)
+		var collision = move_and_collide((current_target.position - position).normalized() * speed * delta)
+		if collision:
+			if collision.collider == current_target:
 				current_state = State.MELEE
+			else:
+				if collision.collider.team != team:
+					current_target = collision.collider
+					current_state = State.MELEE
 	
-func attack(target, delta):
-	#Todo recheck target in range
-	target.move_and_collide((target.position - position).normalized() * delta * 3000)
-	current_cooldown = attack_cooldown
-	current_state = State.MOVING
-	target.attacked()
+func attack_target(delta):
+	if valid_target():
+		#Todo recheck target in range
+		current_target.move_and_collide((current_target.position - position).normalized() * delta * 3000)
+		current_cooldown = attack_cooldown
+		current_state = State.MOVING
+		current_target.attacked()
 	
 func attacked():
 	become_dizzy()
@@ -95,8 +109,15 @@ func become_undizzy():
 	$FrontLegsSprite.animation = "Walk"
 	
 func fall():
-	print(name + " has fallen!")
-	queue_free()
+	# TODO Animations and etc.
+	current_state = State.FALLEN
+	visible = false
+	if get_parent().check_victory():
+		# The opposite team of the one that fell won
+		if team == Team.PLAYER:
+			print("The enemy team won :(")
+		else:
+			print("The player team won!!!")
 
 func _on_Frog_input_event(viewport, event, shape_idx):
 	if current_state == State.PREPARE:
@@ -104,4 +125,15 @@ func _on_Frog_input_event(viewport, event, shape_idx):
 			if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 				dragging = true
 			if event is InputEventMouseButton and !event.pressed and event.button_index == BUTTON_LEFT:
-				dragging = false
+				undrag()
+				
+func undrag():
+	dragging = false
+	if ($Overlap.get_overlapping_bodies().size() == 1
+	and $Overlap.overlaps_area(get_parent().get_parent().get_node("Area2D"))):
+		# Overlaps with only self and the lilypad, this is gross
+		legal_position = true
+	else:
+		legal_position = false
+	print("I can be here: " + str(legal_position))
+		
