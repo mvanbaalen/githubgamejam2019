22108a9fb8a80da2b93774acdb84e7e08412077f
Anthony <midisaurus@gmail.com>
2019-11-30 21:05:11 -0500

    Breed scene setup and code stubs

--- Project/Prepare/Breed/breed.gd
+++ Project/Prepare/Breed/breed.gd
@@ -1,7 +1,57 @@
 extends Control
 
+const FIRST = 0
+const SECOND = 1
+
+var MyFrogs = load('res://MyFrogs/MyFrogs.tscn')
+var frogs_to_breed
+var breeding_is_complete = false
+var tick = 0
+
 func _ready():
+	#Identify which frogs were selected for breeding and load profiles
+	frogs_to_breed = []
+	for frog in MyFrogs.get_children():
+		if frog.is_breeding:
+			frogs_to_breed.append(frog)
+			
+	#Animate/Tween profile nodes
+	$FirstFrogTween.interpolate_property($Node2D, "position",
+	        Vector2(0, 0), Vector2(100, 0), 1,
+	        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$FirstFrogTween.start()
+	$SecondFrogTween.interpolate_property($Node2D, "position",
+	        Vector2(0, 0), Vector2(-100, 0), 1,
+	        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$SecondFrogTween.start()
+	
+	#Remove later
 	$Button.grab_focus()
 
 func _on_Button_pressed():
-	get_tree().change_scene('res://Prepare/Prepare.tscn')
+	if breeding_is_complete:
+		get_tree().change_scene('res://Prepare/Prepare.tscn')
+	
+func _process(delta):
+	#animate visuals
+	tick += delta
+	$Background.texture.noise.period = 64 + 10*sin(PI*tick)
+	$Background2.texture.noise.period = 64 - 10*sin(PI*tick)
+	
+func _on_tween_all_completed():
+	# when tweens finish call finished_breeding() and add newly generated frog
+	generate_new_frog(frogs_to_breed)
+	$FadeIn.show()
+	$FadeIn.fade_in()
+
+func _on_FadeIn_fade_finished():
+	$FrogProfile1.visible = false
+	$FrogProfile2.visible = false
+	$FrogProfileNew.visible = true
+	$FadeIn.hide()
+
+func generate_new_frog(frogs_to_breed):
+	pass
+
+func finished_breeding():
+	pass
